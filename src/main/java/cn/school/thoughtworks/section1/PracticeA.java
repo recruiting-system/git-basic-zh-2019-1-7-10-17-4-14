853271100f80bf42da6b9ba25d58082fb127b471
Chen Meng <94532161@qq.com>
2019-03-20 14:45:13 +0800

    [RWD-9527] Chen Meng: Finished Section1 with test

--- src/main/java/cn/school/thoughtworks/section1/PracticeA.java
+++ src/main/java/cn/school/thoughtworks/section1/PracticeA.java
@@ -1,10 +1,20 @@
 package cn.school.thoughtworks.section1;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
 
 public class PracticeA {
     List<String> collectSameElements(List<String> collection1, List<String> collection2) {
         //实现练习要求，并改写该行代码。
-        return null;
+        if (collection1.isEmpty() || collection2.isEmpty())
+            return null;
+        List<String> Exists = new ArrayList<>(collection1);
+        List<String> noExists = new ArrayList<>(collection1);
+        //rm current exists
+        Exists.removeAll(collection2);
+        noExists.removeAll(Exists);
+        return noExists;
     }
 }
--- src/test/java/cn/school/thoughtworks/section1/PracticeATest.java
+++ src/test/java/cn/school/thoughtworks/section1/PracticeATest.java
@@ -2,7 +2,6 @@ package cn.school.thoughtworks.section1;
 
 import org.junit.Before;
 import org.junit.Test;
-
 import java.util.Arrays;
 import java.util.List;
 
